666b842fff8bc579f8243378bd7c078b601ad7c1
Adil <adil1350@gmail.com>
2019-06-09 12:38:30 +0100

    Action reactors and redux-form reducer added

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,20 +1,43 @@
 import axios from 'axios';
-import { FETCH_USER } from './types';
+import { FETCH_USER, SUBMIT_SURVEY } from './types';
 
 export const fetchUser = () => (dispatch) => {
-	axios.get('/api/current_user').then((res) => {
-    dispatch({
-      type: FETCH_USER,
-      payload: res.data
-    })
-  }).catch((err) => console.log(err));
+	axios
+		.get('/api/current_user')
+		.then((res) => {
+			dispatch({
+				type: FETCH_USER,
+				payload: res.data
+			});
+		})
+		.catch((err) => console.log(err));
 };
 
 export const handleToken = (token) => (dispatch) => {
-	axios.post('/api/stripe', token).then((res) => {
-    dispatch({
-      type: FETCH_USER,
-      payload: res.data
-    })
-  }).catch((err) => console.log(err));
+	axios
+		.post('/api/stripe', token)
+		.then((res) => {
+			dispatch({
+				type: FETCH_USER,
+				payload: res.data
+			});
+		})
+		.catch((err) => console.log(err));
+};
+
+export const submitSurvey = (values, history) => (dispatch) => {
+	console.log(values);
+	axios
+		.post('/api/surveys', values)
+		.then((res) => {
+			dispatch({
+				type: FETCH_USER,
+				payload: res.data
+			});
+			history.push('/surveys');
+		})
+		.catch((err) => console.log(err));
+	return {
+		type: SUBMIT_SURVEY
+	};
 };
